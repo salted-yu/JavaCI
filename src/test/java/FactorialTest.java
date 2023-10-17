dbba060d8846b0fafef6fabf7f4a1dc0b1ce2eb0
Huazi You <2861833656@qq.com>
2023-10-17 11:14:59 +0800

    initial

---
+++ src/test/java/FactorialTest.java
@@ -0,0 +1,18 @@
+//import org.junit.jupiter.api.Assertions.*;
+//import org.junit.jupiter.api.Test;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class FactorialTest {
+
+    @Test
+    public void testFact() {
+        assertEquals(1, Factorial.fact(1));
+        assertEquals(2, Factorial.fact(2));
+        assertEquals(6, Factorial.fact(3));
+        assertEquals(3628800, Factorial.fact(10));
+        assertEquals(2432902008176640000L, Factorial.fact(20));
+    }
+}
